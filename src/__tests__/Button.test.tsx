854a5dc825230726e9c98bc4a339e3a24e56a307
chicrock <jungsungrak@gmail.com>
2019-02-26 08:16:05 +0900

    Add html output test

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { expect } from "chai";
+import { expect as chaiExpect } from "chai";
 import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import sinon from "sinon";
@@ -10,18 +10,26 @@ configure({ adapter: new Adapter() });
 describe("<Button />", () => {
   it("renders a <Button /> components", () => {
     const button = shallow(<Button value="Test" />);
-    expect(button.length).to.equal(1);
+    expect(button.length).toEqual(1);
   });
 
   it("renders with className props", () => {
     const button = shallow(<Button value="Test" className={"asdf"} />);
-    expect(button.find(".asdf").length).to.equal(1);
+    expect(button.find(".asdf").length).toEqual(1);
   });
 
-  it("simulaasdfaskldfoi", () => {
+  it("renders to Button click Event", () => {
     const onClick = sinon.spy();
     const button = shallow(<Button value="Test" onClick={onClick} />);
     button.simulate("click");
-    expect(onClick).to.have.property("callCount", 1);
+    chaiExpect(onClick).to.have.property("callCount", 1);
+  });
+
+  it("renders the output", () => {
+    const button = shallow(<Button value="Test" />);
+    const expectedOutput = '<input type="submit" value="Test"';
+
+    const realOutput = button.html();
+    expect(realOutput.indexOf(expectedOutput) > -1).toEqual(true);
   });
 });
